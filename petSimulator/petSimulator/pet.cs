a4e3a3ab30079924e0096444f9f612744ca4e1fb
Peanut888 <justinlovesborja5678@gmail.com>
2017-02-04 14:53:24 -0800

    Add files via upload

---
+++ petSimulator/petSimulator/pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petSimulator
+{
+    class Pet
+    {
+        public void setScale(int s)
+        {
+            scale = s;
+        }
+        public void setHunger(int h)
+        {
+            hunger = h;
+        }
+        public int scale;
+        public int hunger;
+    }
+
+    class Cow: Pet
+    {
+        public int getScale()
+        {
+            return scale;
+        }
+        public int getHunger()
+        {
+            return hunger;
+        }
+    }
+}
