9339d2e12f7b8498770957399c6d260acd2484b9
Peanut888 <justinlovesborja5678@gmail.com>
2017-02-04 21:50:38 -0800

    New Functionsyou cunt

--- Crimson Code Project/Assets/Scripts/Animal/Hunger.cs
+++ Crimson Code Project/Assets/Scripts/Animal/Hunger.cs
@@ -22,8 +22,6 @@ public class Hunger: Pet
                 hungerCount++;
                 timer = 0.0f;
             }
-
-
         }
 	}
   
--- Crimson Code Project/Assets/Scripts/Animal/Thirst.cs
+++ Crimson Code Project/Assets/Scripts/Animal/Thirst.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Thirst: Pet
 {
+    public int thirstCount = 0;
+    private float timer = 0.0f;
 	// Use this for initialization
-	void Start () {
+	void Start()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update()
+    {
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 10)
+            {
+                thirstCount++;
+                timer = 0.0f;
+            }
+        }
+    }
 }
