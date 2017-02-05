09a71f0e3ee1cdc473edb0a30b05922e3d8633d1
Peanut888 <justinlovesborja5678@gmail.com>
2017-02-04 21:39:25 -0800

    Functions

---
+++ Crimson Code Project/Assets/Scripts/Animal/Cow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cow: Pet
+{
+    public double getScale()
+    {
+        return scale;
+    }
+    public int getHunger()
+    {
+        return hunger;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Crimson Code Project/Assets/Scripts/Animal/Hunger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hunger: Pet
+{
+    public int hungerCount = 0;
+    private float timer = 0.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		while (true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= 10)
+            {
+                hungerCount++;
+                timer = 0.0f;
+            }
+
+
+        }
+	}
+  
+}
