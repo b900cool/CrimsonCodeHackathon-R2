a0beb1be29a3ca201e6a1c70569cdcc8854042c9
b900cool <b900cool@gmail.com>
2017-02-04 16:21:46 -0800

    Motion Script
    Allows the attached object to move to a random location within a certain
    area.

---
+++ Crimson Code Project/Assets/Scripts/AI Motion/Wander.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+    public Vector3 destination = new Vector3(1, 1, 1);
+    public float speed = 1;
+    public float waitTime = 3f;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        destination = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float step = speed * Time.deltaTime;
+        
+        move(step);
+	}
+
+    void move(float step)
+    {
+        if (transform.position == destination)
+        {
+            
+            StartCoroutine(wait());
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+    }
+
+    
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (transform.position == destination)
+        {
+            destination = new Vector3(Random.Range(-20, 20), 0.5f, Random.Range(-20, 20));
+        }
+    }   
+
+}
